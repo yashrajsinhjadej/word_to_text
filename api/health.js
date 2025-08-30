be25ba1e8108fb50b0a5ecc00f5afac5dec57c7e
yashrajsinhjadej <jadejayashrajsinh725@gmail.com>
2025-08-30 21:42:10 +0530

    force vercel

--- api/health.js
+++ api/health.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
   }
 
   res.status(200).json({
-    message: 'Word-to-Text API Server is running!',
+    message: 'Word-to-Text API Server is running!!!!',
     status: 'Active',
     timestamp: new Date().toISOString()
   });
